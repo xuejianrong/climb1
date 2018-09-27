ce4af9a3762505aa9bed9429901a688cc6b44bb3
xuejianrong <xuejianrong@yuntutv.net>
2018-09-27 20:11:47 +0800

    增加小兔子运动

--- assets/Script/GameView.js
+++ assets/Script/GameView.js
@@ -7,14 +7,19 @@ cc.Class({
     ctrlView: cc.Node,
     stair: cc.Prefab,
     stairs: [cc.Node],
+    player: cc.Node,
     primaryHeight: 0, // scale为1时stair的高度（包含间距），第一个看不见
     firstEndScaleX: .8,
     firstEndScaleY: .5,
     firstY: 200,
-    firstEndY: 0, // 113.75  ,  speed = (firstY - firstEndY) / .6, .6为跳跃一次所需的时间（初始）
-    speed: 143.75, // px/s
+    firstEndY: 0, // 113.75  ,  firstY = speed * .6 + firstEndY, .6为跳跃一次所需的时间（初始）
+    speed: 143.75, // stair的运动速度 px/s ，作为游戏整体速度（难度的控制）
     scaleXChange: .13,
     scaleYChange: .18,
+    initX: 0,
+    initY: -89.8,
+    jumpHeight: 350,
+    moveTime: 0, // player每次起跳之后的时间
   },
 
   onLoad() {
@@ -25,8 +30,11 @@ cc.Class({
 
   update(dt) {
     if (this.isStart) {
+      // 上下一趟的时间
+      const dur = (this.firstY - this.firstEndY) / this.speed;
+
       this.stairs.forEach((stair, index) => {
-        const dur = (this.firstY - this.firstEndY) / this.speed;
+        // stair运动
         if (index === 0) {
           stair.y -= this.speed * dt;
           stair.scaleX += (this.firstEndScaleX / dur) * dt;
@@ -40,16 +48,39 @@ cc.Class({
           stair.y = prevStair.y - prevHalf - half;
         }
       });
-      if (this.stairs[0].y <= this.firstEndY) {
-        // this.isStart = false;
-        // 把stairs中最后一个放到第一个
-        const last = this.stairs.pop();
-        this.stairs.unshift(last);
-        // 重置y坐标和缩放比例
-        last.scaleX = 0;
-        last.scaleY = 0;
-        last.y = this.firstY;
-      }
+
+      // player运动
+      this.moveTime += dt;
+      const t = dur / 2;
+      const a = (this.jumpHeight * 2) / (t * t); // 加速度，反向
+      const v = a * t; // 初始速度（最大速度，正向）
+      const vt = v - (a * this.moveTime); // 瞬时速度（向量）
+      this.player.y += vt * dt;
+    }
+  },
+
+  lateUpdate() {
+    // stair到了临界位置
+    if (this.stairs[0].y <= this.firstEndY) {
+      // this.isStart = false; // 用于调试
+      // 把stairs中最后一个放到第一个
+      const last = this.stairs.pop();
+      this.stairs.unshift(last);
+      // 重置y坐标和缩放比例
+      last.scaleX = 0;
+      last.scaleY = 0;
+      last.y = this.firstY;
+
+      // 调整player位置
+      this.initPlayer();
+      // 清零运动时间
+      this.moveTime = 0;
+    }
+
+    // player到了顶点
+    if (this.player.y >= this.initY + this.jumpHeight) {
+      // 调整player位置
+      this.player.y = this.initY + this.jumpHeight;
     }
   },
 
@@ -73,9 +104,19 @@ cc.Class({
         if (i === 1) {
           this.firstEndY = newStair.y;
         }
+        if (i === 3) {
+          this.initY = newStair.y;
+        }
+        this.initPlayer();
       }
       i += 1;
     }
+  },
+
+  initPlayer() {
+    this.player.x = this.initX;
+    this.player.y = this.initY;
+    this.player.setSiblingIndex(10);
   },
 
   startGame() {
